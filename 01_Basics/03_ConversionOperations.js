06feeb173e5ce201416dc4d23e851f9062e8b4f1
Raja Muhammad Bilal Arshad <bilalarshad.code@gmail.com>
2025-07-03 02:00:13 +0000

    Comparisons and concversions

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,7 +1,7 @@
 //let score ="33abc"
-//let score2 =false
-console.log(typeof score);
-console.log(typeof(score));
+// //let score2 =false
+// console.log(typeof score);
+// console.log(typeof(score));
 
 //let valueinNumber =Number(score)
 //console.log(typeof(valueinNumber));
@@ -16,14 +16,44 @@ console.log(typeof(score));
 // true => 1
 // false => 0
 
-let LoggedIn = ""
+// let LoggedIn = ""
 
-let New_Number = Boolean(LoggedIn)
+// let New_Number = Boolean(LoggedIn)
 
-console.log(New_Number);
-console.log(typeof New_Number);
+// console.log(New_Number);
+// console.log(typeof New_Number);
 // 1=> true    0=> false
 // Conversion to Boolean is as following
 // "" empty means 0 =>false
 // "2322w" some value in the string gives true/1 value
 // 
+
+//*********OPERATION******** 
+// console.log(5-7);
+// console.log(5+7);
+// console.log(5/7);
+// console.log(5%7);
+// console.log(5*7);
+
+let str1 ="Muhammad"
+let str2 =" Bilal"
+let str3 = str1+str2
+console.log(str3);
+
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 +"3");
+console.log("1" + 2 + 3);
+console.log(1 + "2" + 3);
+
+console.log(+true);// => outputs 1
+//console.log(true+); => Is not correct format
+
+console.log(+""); // => outputs 0
+
+let counter =20
+counter++;
+++counter;
+console.log(counter);
---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,20 @@
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(3 >= 2);
+console.log(3 <= 2);
+console.log(3 == 2);
+
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >=0);
+
+
+
+console.log(undefined == 0);
+console.log(undefined >0 );
+console.log(undefined <0);
+
+
+// === => strict checking, Compares  the datatype as well as the values in them
+
+console.log("2" === 2);   // gives asnwers as false because they both are different datatype and === strictly checks everything
