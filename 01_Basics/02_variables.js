05101f329886175ee83893a028b997bacd096017
Raja Muhammad Bilal Arshad <bilalarshad.code@gmail.com>
2025-07-03 01:14:50 +0000

    We have learned Conversions

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,27 @@
+"use strict";
+console.log(3+5);
+
+
+let name ="Muhammad Bilal"
+let age =3
+let state;
+
+// number => 2^52/53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+//   Original DOcumentation of Javascript is ECMA script on tc39.es(All the standards of JavaScript are written here) but people usually explore mdn by Mozilla
---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,29 @@
+//let score ="33abc"
+//let score2 =false
+console.log(typeof score);
+console.log(typeof(score));
+
+//let valueinNumber =Number(score)
+//console.log(typeof(valueinNumber));
+//console.log(valueinNumber);
+
+ //let testnumber2 = Number(score2)
+//console.log(testnumber2);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let LoggedIn = ""
+
+let New_Number = Boolean(LoggedIn)
+
+console.log(New_Number);
+console.log(typeof New_Number);
+// 1=> true    0=> false
+// Conversion to Boolean is as following
+// "" empty means 0 =>false
+// "2322w" some value in the string gives true/1 value
+// 
